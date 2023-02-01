627e20b0530a742517e36357bd8bc83ceba7aef8
David <davidalejangp@gmail.com>
2023-02-01 17:41:26 -0500

    -Autenticacion de usario -mensaje en pantalla paradiferentes estados(Cargando.., logeado, logeado no registrado, no logeado)

--- Prestamos/src/routes/LoginView.jsx
+++ Prestamos/src/routes/LoginView.jsx
@@ -1,7 +1,38 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
 import { auth } from "../firebase/firebase";
+import { useEffect, useState } from "react";
 
+/*
+  Stages:
+  0: initiated
+  1: loading
+  2: login completed
+  3: login but no username
+  4: not logged
+*/
 export default function LoginView() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [state, setCurrentState] = useState(0);
+
+  useEffect(() => {
+    setCurrentState(1);
+    onAuthStateChanged(auth, handleUserStateChanged);
+  }, []);
+
+  function handleUserStateChanged(user) {
+    setCurrentState(3);
+    if (user) {
+      console.log(user.displayName);
+    } else {
+      setCurrentState(4);
+      console.log("No hay nadie autenticado");
+    }
+  }
+
   async function handleOnClick() {
     const googleProvider = new GoogleAuthProvider();
     await signInWithGoogle(googleProvider);
@@ -15,10 +46,15 @@ export default function LoginView() {
       console.error(error);
     }
   }
-
-  return (
-    <div>
-      <button onClick={handleOnClick}>Ingresar con Google</button>
-    </div>
-  );
+  if (state==3) {
+    return <div>Estas autenticado pero no registrado</div>
+  }
+  if (state==4) {
+    return (
+      <div>
+        <button onClick={handleOnClick}>Ingresar con Google</button>
+      </div>
+    );
+  }
+  return <div>Cargando...</div>
 }
